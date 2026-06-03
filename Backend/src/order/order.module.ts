596c2c486bcede3e9bb360bc6e599c9faf65bd5d
Andhika4s <andhikadwisanjaya41@gmail.com>
2026-06-03 14:09:11 +0700

    fix be

--- Backend/src/order/order.module.ts
+++ Backend/src/order/order.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ProductModule } from 'src/product/product.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [UserModule, ProductModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
